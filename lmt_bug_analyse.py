048de1f9479535adc3f261224cdb6762c5b54a49
haixiaohai <zjhong8259@gmail.com>
2024-10-15 10:23:16 +0800

    20241015：完成基础功能

--- lmt_bug_analyse.py
+++ lmt_bug_analyse.py
@@ -6,9 +6,9 @@ output_file = 'D:\安恒信息\LMT\现网问题\LMT_BUG_CURRENT_STATUS.xlsx'
 input_file2 = 'D:\安恒信息\LMT\现网问题\LMT_BUG_LAST_WEEK_STATUS.xlsx'
 
 bug_sheets = [
-    ("2023 Q2现网问题闭环","问题链接"), 
-    ("2023 Q3现网问题闭环","问题链接"),
-    ("2023 Q4现网问题闭环","问题链接"),
+    # ("2023 Q2现网问题闭环","问题链接"), 
+    # ("2023 Q3现网问题闭环","问题链接"),
+    # ("2023 Q4现网问题闭环","问题链接"),
     ("2024 Q1现网问题闭环","问题链接"),
     ("2024 Q2现网问题闭环","禅道链接"),
     ("2024 Q3现网问题闭环","禅道链接"),
@@ -58,7 +58,11 @@ def get_bug_status(bug_ids):
     '''
 
     rst_df = pandas.DataFrame(columns=["Bug编号", "Bug标题", "责任人", "是否紧急", "Bug状态"])
-    engine = create_engine(f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}') # 创建数据库连接
+    try:
+        engine = create_engine(f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}') # 创建数据库连接
+    except:
+        print("数据库连接失败")
+        return
     for bug in bug_ids.values: # bug是一个list，包含bug id和是否紧急
         mysql_bug_df = pandas.read_sql_query(f'SELECT id,title,assignedto,status,substatus FROM zt_bug WHERE id={bug[0]}', engine)  # 根据bug id查询bug状态，返回一个df
         # 需要根据BUG的status和substatus公共判断BUG是否关闭
@@ -80,23 +84,27 @@ def compare_df(df1, df2):
 if __name__ == '__main__':
     bug_ids = get_bugs(file=input_file, bug_sheets=bug_sheets)
 
-    last_week_bug_status_df = pandas.read_excel(input_file2) # 读取上一周BUG状态数据
-    current_bug_status_df = get_bug_status(bug_ids) # 获取当前bug状态数据
+    last_week_bug_status_df = pandas.read_excel(input_file2).set_index('Bug编号') # 读取上一周BUG状态数据，Bug编号是索引
+    current_bug_status_df = get_bug_status(bug_ids).set_index('Bug编号') # 获取当前bug状态数据，Bug编号是索引
 
     # 对比BUG编号，检查是否有新增，若有新增，将新增的BUG保存到一个DataFrame
-    mask = current_bug_status_df['Bug编号'].astype(int).isin(last_week_bug_status_df['Bug编号'].astype(int))
+    # 需要修改为根据Bug ID判断，因为Bug可以修改
+    mask = current_bug_status_df['Bug标题'].isin(last_week_bug_status_df['Bug标题'])
     increase_bug_df = current_bug_status_df[~mask]
 
     # 对边BUG状态，将有变化的BUG保存到一个DataFrame
-    current_bug_status_df.set_index('Bug编号', inplace=False)
-    last_week_bug_status_df.set_index('Bug编号', inplace=False)
-
-
-
+    change_bug_df = pandas.DataFrame(columns=["Bug编号", "Bug标题", "责任人", "是否紧急", "Bug状态"], index=['Bug编号'])
+    for bug_id in last_week_bug_status_df.index:
+        # 根据Bug编号判断上周和当前的Bug状态是否一致，若一致则跳过，若不一致则保存到DataFrame
+        if str(bug_id) in current_bug_status_df.index:
+            if last_week_bug_status_df.loc[bug_id, 'Bug状态'] != current_bug_status_df.loc[str(bug_id), 'Bug状态']:
+                change_bug_df.loc[len(change_bug_df)] = current_bug_status_df.loc[str(bug_id)]
+    
     #最新的BUG状态数据写入文件之前将原来的文件备份到上周数据中（LAST WEEK STATUS)
     # pandas.read_excel(output_file).to_excel(input_file2, index=False)
     
     # 将最新数据写入文件
     with pandas.ExcelWriter(output_file) as writer:
-        current_bug_status_df.to_excel(writer, sheet_name="当前所有问题", index=False)
-        increase_bug_df.to_excel(writer, sheet_name="新增问题", index=False)
+        current_bug_status_df.to_excel(writer, sheet_name="当前所有问题", index_label='Bug编号', index=True)
+        increase_bug_df.to_excel(writer, sheet_name="新增问题", index_label='Bug编号', index=True)
+        change_bug_df.to_excel(writer, sheet_name="状态变更问题", index_label='Bug编号', index=True)
